f2bde6a53ab03905619eb5bf494dcd84386cbc0c
Juergen Pabel <juergen@pabel.net>
2025-10-01 00:04:25 +0200

    Update HTML parsing for redirect to match new sportschau.de content format

--- redirect.py
+++ redirect.py
@@ -17,13 +17,11 @@ def do_redirect(html, filter_date, filter_comp, filter_team):
 			if re.search(r"\bcompetition-head\b", classes):
 				current_comp = element.find(".//img").get("title")
 			if re.search(r"\bround-head\b", classes):
-				current_round = element.find(".//div[@class='match-round']").text
+				current_round = element.find(".//h6").text
 			if filter_team is None:
 				if re.search(r"\bactivity-head\b", classes):
 					if filter_comp is None or filter_comp == current_comp:
-						conf_live = element.find(".//div[@class='match-activity']")
-						if conf_live is not None:
-							location = conf_live.get("data-audiolivestream")
+						location = element.get("data-audiolivestream")
 			else:
 				if re.search(r"\bmatch\b", classes):
 					if filter_comp is None or filter_comp == current_comp:
